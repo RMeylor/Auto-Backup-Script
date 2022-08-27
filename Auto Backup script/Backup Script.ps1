eddc370e47df3ee0b9286b134d922e479766b9ce
RMeylor <112234783+RMeylor@users.noreply.github.com>
2022-08-27 10:23:16 -0700

    Add files via upload

---
+++ Auto Backup script/Backup Script.ps1
@@ -0,0 +1,4 @@
+﻿$folder = "C:\Users\$Env:USERNAME\Documents\AutoBackup"
+$backup = "G:\My Drive"
+
+Copy-Item -Path $backup -Destination $folder -force -recurse
---
+++ Auto Backup script/File Creation 1.0.0.ps1
@@ -0,0 +1,8 @@
+﻿$folder = "C:\Users\$Env:USERNAME\Documents\AutoBackup"
+
+cd $folder
+New-Item ".\Download Path.txt"
+Set-Content ".\Download Path.txt" 'G:\My Drive'
+
+New-Item ".\Upload Path.txt"
+Set-Content ".\Upload Path.txt" $folder
